a21b04ddae3c5952719bf619093d1adff8d9f7b1
Jhon Frank Alvarado Echigoyen <jhonfrank.ae@outlook.com>
2024-02-24 17:41:42 -0500

    Final

--- Ejercicio1Api/Program.cs
+++ Ejercicio1Api/Program.cs
@@ -7,6 +7,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 app.UseSwagger();
@@ -14,8 +16,13 @@ app.UseSwaggerUI();
 
 app.UseAuthorization();
 
+// global cors policy
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .SetIsOriginAllowed(origin => true) // allow any origin
+    .AllowCredentials()); // allow credentials
+
 app.MapControllers();
 
 app.Run();
-
-Console.WriteLine("Server running...");
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Ejercicio1Api
+
+Para ejecutar en docker:
+
+Port: 5010
+
+ENV:
+    - ASPNETCORE_URLS=http://+:5010
+    - ConnectionStrings__DefaultConnection=Server=server; Database=db;User Id=sa;Password=password;
+
+## Docker exec
+
+docker build -t api .
+docker run --network=nt0 -p 5010:5010 --name api-1 api
